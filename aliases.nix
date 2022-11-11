4d0f7cdacfc91483f0dbea75215fcffc4eccf61a
uonr <34709816+uonr@users.noreply.github.com>
2022-11-12 02:31:23 +0900

    feat: aliases module

---
+++ aliases.nix
@@ -0,0 +1,22 @@
+{ pkgs, lib, config, ... }:
+with lib;
+let
+  cfg = config.home.my;
+  isLinux = pkgs.stdenv.isLinux;
+  isDarwin = pkgs.stdenv.isDarwin;
+  common = { };
+  linuxAliases = optionalAttrs isLinux {
+    doco = "docker-compose";
+    switch = "sudo nixos-rebuild switch --flake";
+    sys = "systemctl";
+    jou = "journalctl";
+  };
+  darwinAliases =
+    optionalAttrs isDarwin { switch-home = "home-manager switch --flake"; };
+  shellAliases = common // linuxAliases // darwinAliases;
+in {
+  config = lib.mkIf cfg.enable {
+    programs.zsh.shellAliases = shellAliases;
+    programs.bash.shellAliases = shellAliases;
+  };
+}
--- prompt.nix
+++ prompt.nix
@@ -1,34 +1,36 @@
 { config, lib, ... }:
 let cfg = config.home.my;
 in {
-  programs.starship = lib.mkIf (!cfg.lite) {
-    enable = !cfg.lite;
-    settings = {
-      hostname.ssh_symbol = "{SSH} ";
-      rust = { symbol = "𝓡 "; };
-      nix_shell = {
-        symbol = "❆ ";
-        disabled = true;
+  config = lib.mkIf cfg.enable {
+    programs.starship = lib.mkIf (!cfg.lite) {
+      enable = !cfg.lite;
+      settings = {
+        hostname.ssh_symbol = "{SSH} ";
+        rust = { symbol = "𝓡 "; };
+        nix_shell = {
+          symbol = "❆ ";
+          disabled = true;
+        };
+        lua = { symbol = "☽"; };
+        nodejs = {
+          symbol = "⬢ ";
+          disabled = true;
+        };
+        python = { symbol = "Py "; };
+        directory = {
+          read_only = " [RO]";
+          read_only_style = "yellow";
+        };
+        terraform = { disabled = true; };
+        battery = {
+          full_symbol = "满 ";
+          charging_symbol = "输 ";
+          discharging_symbol = "断 ";
+          unknown_symbol = "电 ";
+          empty_symbol = "乏 ";
+        };
+        package.disabled = true;
       };
-      lua = { symbol = "☽"; };
-      nodejs = {
-        symbol = "⬢ ";
-        disabled = true;
-      };
-      python = { symbol = "Py "; };
-      directory = {
-        read_only = " [RO]";
-        read_only_style = "yellow";
-      };
-      terraform = { disabled = true; };
-      battery = {
-        full_symbol = "满 ";
-        charging_symbol = "输 ";
-        discharging_symbol = "断 ";
-        unknown_symbol = "电 ";
-        empty_symbol = "乏 ";
-      };
-      package.disabled = true;
     };
   };
 }
